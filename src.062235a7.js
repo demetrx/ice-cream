6c31a42778fdcdda19a5437317c801178e56be00
demetrx <91727549+demetrx@users.noreply.github.com>
2022-01-15 09:23:23 +0000

    Deploying to gh-pages from @ demetrx/ice-cream@c7dfa40af50eaefaf01b690f5907364d2b07ae1a 🚀

--- src.062235a7.js
+++ src.062235a7.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/ice-cream/src.82169e1e.js.map
+//# sourceMappingURL=/ice-cream/src.062235a7.js.map
