b8ee94abaa902be5c36dc00481798c71a2041edb
demetrx <mcbright7447@gmail.com>
2022-01-17 18:47:50 +0200

    product switch

--- src/js/overlay.js
+++ src/js/overlay.js
@@ -1,14 +1,42 @@
 (() => {
     const refs = {
-      openOverlayBtn: document.querySelector('.product-list__button'),
-      closeOverlayBtn: document.querySelector('.overlay__close'),
-      overlay: document.querySelector('.overlay'),
+      openOverlayBtn: document.querySelector('.product__switch--a'),
+      closeOverlayBtn: document.querySelector('.product__close--a'),
+      overlay: document.querySelector('.overlay--a'),
     };
   
     refs.openOverlayBtn.addEventListener('click', toggleOverlay);
     refs.closeOverlayBtn.addEventListener('click', toggleOverlay);
   
     function toggleOverlay() {
-      refs.overlay.classList.toggle('overlay--shown');
+      refs.overlay.classList.toggle('is-hidden');
+    }
+})();
+  (() => {
+    const refs = {
+      openOverlayBtn: document.querySelector('.product__switch--b'),
+      closeOverlayBtn: document.querySelector('.product__close--b'),
+      overlay: document.querySelector('.overlay--b'),
+    };
+  
+    refs.openOverlayBtn.addEventListener('click', toggleOverlay);
+    refs.closeOverlayBtn.addEventListener('click', toggleOverlay);
+  
+    function toggleOverlay() {
+      refs.overlay.classList.toggle('is-hidden');
+    }
+})();
+  (() => {
+    const refs = {
+      openOverlayBtn: document.querySelector('.product__switch--c'),
+      closeOverlayBtn: document.querySelector('.product__close--c'),
+      overlay: document.querySelector('.overlay--c'),
+    };
+  
+    refs.openOverlayBtn.addEventListener('click', toggleOverlay);
+    refs.closeOverlayBtn.addEventListener('click', toggleOverlay);
+  
+    function toggleOverlay() {
+      refs.overlay.classList.toggle('is-hidden');
     }
   })();
