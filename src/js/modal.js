96b736f1f995419d4507816e77817970ca0f2e06
demetrx <mcbright7447@gmail.com>
2022-01-18 16:01:20 +0200

    feature/about-modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -14,4 +14,19 @@
       refs.modal.classList.toggle('backdrop--is-hidden');
     }
 })();
+
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-about-open]'),
+      closeModalBtn: document.querySelector('[data-modal-about-close]'),
+      modal: document.querySelector('[data-modal-about]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('backdrop--is-hidden');
+    }
+})();
   
