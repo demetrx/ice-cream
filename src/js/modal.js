28f313f0ee44eb05c09bf832c48b2158d62ab2b0
demetrx <mcbright7447@gmail.com>
2022-01-18 13:11:35 +0200

    bug-fix/modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,12 +1,14 @@
 (() => {
     const refs = {
       openModalBtn: document.querySelector('[data-modal-open]'),
+      openModalBtnTablet: document.querySelector('[data-modal-open-tablet]'),
       closeModalBtn: document.querySelector('[data-modal-close]'),
       modal: document.querySelector('[data-modal]'),
     };
   
     refs.openModalBtn.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.openModalBtnTablet.addEventListener('click', toggleModal);
   
     function toggleModal() {
       refs.modal.classList.toggle('backdrop--is-hidden');
